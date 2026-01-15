1d0d04a09c5207f308f58e0a5371d6324b2050c1
Ionut <ionut.mihalcea@movenext.fr>
2026-01-15 23:51:12 +0100

    chore: delete unused function

--- src/Helpers/Extensions.cs
+++ src/Helpers/Extensions.cs
@@ -1,26 +1,16 @@
 using System;
 using System.Collections.Generic;
+// ReSharper disable InconsistentNaming
 
 namespace HierarchicalStateMachine.Helpers
 {
     public static class Extensions
     {
         public static IDictionary<K, V> AddOrUpdate<K, V>(this IDictionary<K, V> @this, K key, Func<K, V> add,
-            Func<K, V, V> update)
-        {
-            if (@this.ContainsKey(key))
-                @this[key] = update(key, @this[key]);
-            else
-                @this[key] = add(key);
-
-            return @this;
-        }
-        
-        public static IDictionary<K, V> AddOrUpdate<K, V>(this IDictionary<K, V> @this, K key, Func<K, V> add,
             Action<K, V> update)
         {
-            if (@this.ContainsKey(key))
-                update(key, @this[key]);
+            if (@this.TryGetValue(key, out var thi))
+                update(key, thi);
             else
                 @this[key] = add(key);
 
